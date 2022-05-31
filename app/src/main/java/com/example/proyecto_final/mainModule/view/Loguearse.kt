a3da9c0a392bd4cc8344d01b633b0e8caae98ede
gabri185 <gdominguezreyes@danielcastelao.org>
2022-05-31 10:06:37 +0200

    Terminado el loin y el registro y esta echo el login biometrico

--- app/src/main/java/com/example/proyecto_final/mainModule/view/Loguearse.kt
+++ app/src/main/java/com/example/proyecto_final/mainModule/view/Loguearse.kt
@@ -1,15 +1,153 @@
 package com.example.proyecto_final.mainModule.view
 
+import android.app.ProgressDialog
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import com.example.proyecto_final.databinding.ActivityLoguearseBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.util.concurrent.Executor
 
 class Loguearse: AppCompatActivity() {
     private lateinit var binding: ActivityLoguearseBinding
+    lateinit var progress: ProgressDialog
+    lateinit var firebaseauth: FirebaseAuth
+    private var canAuthenticate = false
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var executor: Executor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoguearseBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setup()
+        //setupAuth()
+        authenticate()
+
+        firebaseauth = FirebaseAuth.getInstance();
+        progress = ProgressDialog(this)
+    }
+
+    private fun setupAuth() {
+        if (BiometricManager.from(this).canAuthenticate(
+                BiometricManager.Authenticators.BIOMETRIC_STRONG
+                        or BiometricManager.Authenticators.DEVICE_CREDENTIAL
+            ) == BiometricManager.BIOMETRIC_SUCCESS
+        ) {
+
+            canAuthenticate = true
+
+            promptInfo = BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Autenticación biométrica")
+                .setSubtitle("Autentícate utilixando el sensor biométrico")
+                .setAllowedAuthenticators(
+                    BiometricManager.Authenticators.BIOMETRIC_STRONG
+                            or BiometricManager.Authenticators.DEVICE_CREDENTIAL
+                )
+                .build()
+        }
+    }
+
+    private fun authenticate() {
+
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(
+                    errorCode: Int,
+                    errString: CharSequence
+                ) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(
+                        applicationContext,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult
+                ) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(
+                        applicationContext,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT
+                    )
+                        .show()
+
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(
+                        applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Autenticación biométrica")
+            .setSubtitle("Autentícate utilixando el sensor biométrico")
+            .setAllowedAuthenticators(
+                BiometricManager.Authenticators.BIOMETRIC_STRONG or
+                        BiometricManager.Authenticators.DEVICE_CREDENTIAL
+            )
+            .build()
+
+        // Prompt appears when user clicks "Log in".
+        // Consider integrating with the keystore to unlock cryptographic operations,
+        // if needed by your app.
+
+        binding.btLogin.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+        }
+    }
+
+    private fun setup() {
+        binding.btLogin.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        //Conseguimos el email y la contraseña de las cajas de texto
+        var email: String = binding.tvUsuario.text.toString()
+        val contraseña: String = binding.tvContraseA.text.toString()
+
+        //Comproba,os que las cajas de texto no estan vacias
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Tienes que ingresar tu email", Toast.LENGTH_LONG).show();
+        }
+        if (TextUtils.isEmpty(contraseña)) {
+            Toast.makeText(this, "Tienes que ingresar tu contraseña", Toast.LENGTH_LONG).show();
+        }
+
+        progress.setMessage("Logueandose")
+        progress.show()
+
+        firebaseauth.signInWithEmailAndPassword(email,contraseña)
+            .addOnCompleteListener (this){ task ->
+                if (task.isSuccessful){
+                    Toast.makeText(
+                        this, "Se ha logueado con exito" + binding.tvUsuario.text,
+                        Toast.LENGTH_LONG
+
+                    ).show();
+                }else{
+                    Toast.makeText(
+                        this, "No se pudo loguear" + binding.tvUsuario.text,
+                        Toast.LENGTH_LONG
+
+                    ).show();
+                }
+                progress.dismiss()
+            }
     }
 }
--- app/src/main/java/com/example/proyecto_final/mainModule/view/Registrarse.kt
+++ app/src/main/java/com/example/proyecto_final/mainModule/view/Registrarse.kt
@@ -1,23 +1,78 @@
 package com.example.proyecto_final.mainModule.view
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyecto_final.databinding.ActivityRegistrarseBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Registrarse: AppCompatActivity() {
     private lateinit var binding: ActivityRegistrarseBinding
-
+    lateinit var progress: ProgressDialog
+    lateinit var firebaseauth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistrarseBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setup()
+
+        firebaseauth= FirebaseAuth.getInstance();
+        progress = ProgressDialog(this)
+
     }
 
     private fun setup() {
+
+
         binding.btRegistrarse.setOnClickListener {
-            startActivity(Intent(this, Datos::class.java))
+            comprobarContraseña()
         }
+    }
+
+    private fun comprobarContraseña() {
+        if (binding.tvContraseA.text.toString() == binding.tvContraseAR.text.toString()) {
+            registrarUsuario()
+        }else{
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_LONG).show();
+        }
+    }
+    private fun registrarUsuario() {
+        //Conseguimos el email y la contraseña de las cajas de texto
+        var email: String = binding.tvUsuario.text.toString()
+        val contraseña: String = binding.tvContraseA.text.toString()
+
+        //Comproba,os que las cajas de texto no estan vacias
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Tienes que ingresar tu email", Toast.LENGTH_LONG).show();
+        }else{
+            if (TextUtils.isEmpty(contraseña)) {
+                Toast.makeText(this, "Tienes que ingresar tu contraseña", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        progress.setMessage("Realizando registro en linea")
+        progress.show()
+
+
+        firebaseauth.createUserWithEmailAndPassword(email, contraseña)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(
+                        this, "Se ha registrado el usuario con el email" + binding.tvUsuario.text,
+                        Toast.LENGTH_LONG
+
+                    ).show();
+                    startActivity(Intent(this, Datos::class.java))
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this, "No se pudo registrar el usuario", Toast.LENGTH_LONG)
+                        .show();
+                }
+                progress.dismiss()
+            }
     }
 }
